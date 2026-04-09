3c8c3bfa5102a37bc6ea84ad1fc88e7295088269
Gabriele-Cardosi <gabriele.cardosi@ibm.com>
2026-04-09 11:50:20 +0200

    [incubator-kie-issues#2029] Fixing kie-tools build

--- packages/drools-and-kogito/env/index.js
+++ packages/drools-and-kogito/env/index.js
@@ -52,7 +52,7 @@ module.exports = composeEnv([rootEnv], {
       description: "Git repository URL for Kogito Apps",
     },
     DROOLS_AND_KOGITO__kogitoAppsRepoGitRef: {
-      default: "8b4c14712f21fee4ef15b53ca4ad3fee93349a19",
+      default: "5f1d25dabcecf8cf63344028e43edbfd25760a72",
       description: "Git ref for the Kogito Apps repository (SHA, branch, or tag)",
     },
     DROOLS_AND_KOGITO__skip: {
